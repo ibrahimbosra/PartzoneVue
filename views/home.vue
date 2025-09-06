162dc2d1efae18e858e0152026641865468fe99b
ibrahimbosra <109563092+ibrahimbosra@users.noreply.github.com>
2025-09-06 18:33:23 +0300

    Create home.vue

---
+++ views/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="home-page">
+    <Hero />
+    <RotatingBanners />
+  </div>
+</template>
+
+<script>
+import Hero from '@/components/Hero'
+import RotatingBanners from '@/components/RotatingBanners'
+
+export default {
+  components: {
+    Hero,
+    RotatingBanners
+  }
+}
+</script>
